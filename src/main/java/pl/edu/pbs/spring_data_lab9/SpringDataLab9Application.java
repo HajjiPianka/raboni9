6e9a14c3650cdb6237b90fc1220d32ea1c554a4b
Pawelo112 <elopawelo112@gmail.com>
2024-05-06 17:57:01 +0200

    Lab 9

---
+++ src/main/java/pl/edu/pbs/spring_data_lab9/SpringDataLab9Application.java
@@ -0,0 +1,14 @@
+package pl.edu.pbs.spring_data_lab9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataLab9Application
+{
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataLab9Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edu/pbs/spring_data_lab9/SpringDataLab9ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pbs.spring_data_lab9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataLab9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
